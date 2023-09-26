9d3cea56cebe55d54bfa18755ac00310749511e9
Yan Sabarense <sabarensey@gmail.com>
2023-09-26 11:39:20 -0300

    [UPDATE] add players.csv

--- TP02/Q01/Principal.java
+++ TP02/Q01/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) throws Exception {
         Jogador jogador = new Jogador();
         try{
-            jogador.ler("C:\\Faculdade\\AEDs-II\\TP02\\Players.csv");
+            jogador.ler("C:\\Users\\yan.silva\\Desktop\\AEDs-II\\TP02\\Q01\\players.csv");
         }catch (Exception e){
             e.printStackTrace();
         }
